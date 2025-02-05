3c9cce593413d5127be5b103b2d7439d15cd80a7
Roman Bakaldin <gbakaldin@gmail.com>
2025-02-05 20:48:43 +0300

    Clean up code

--- fsm-router/src/test/java/io/github/amayaframework/fsm/MachineRouterTest.java
+++ fsm-router/src/test/java/io/github/amayaframework/fsm/MachineRouterTest.java
@@ -10,7 +10,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public final class MachineRouterTest {
     private static final RouterFactory DYNAMIC_FACTORY = new MachineRouterFactory(new BytecodeMachineFactory());
