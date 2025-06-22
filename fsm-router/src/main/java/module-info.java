9285e11d388518a53c26643494212090059e82fe
Roman Bakaldin <gbakaldin@gmail.com>
2025-06-23 01:27:57 +0300

    Clean up dependencies

--- fsm-router/src/main/java/module-info.java
+++ fsm-router/src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module io.github.amayaframework.router.fsm {
     // Imports
     requires com.github.romanqed.jsm;
-    requires io.github.amayaframework.router;
     requires io.github.amayaframework.path;
+    requires io.github.amayaframework.router;
     requires io.github.amayaframework.tokenize;
     // Exports
     exports io.github.amayaframework.router.fsm;
