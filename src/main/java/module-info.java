db798967c606a81846ee81f505c133f31feb8ba1
Roman Bakaldin <gbakaldin@gmail.com>
2025-09-04 01:55:41 +0300

    Cleanup module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,22 @@
 module amayaframework.routing {
     // TODO Remove dep deps
     requires amayaframework.core;
-    requires amayaframework.options;
     requires amayaframework.tomcat;
-    requires amayaframework.router;
-    requires amayaframework.filter;
-    requires amayaframework.path;
-    requires com.github.romanqed.jsync;
-    requires amayaframework.router.tree;
-    requires amayaframework.path.parser;
-    requires amayaframework.tokenize;
     // Imports
+    // Basic dependencies
+    requires com.github.romanqed.jtype;
+    requires com.github.romanqed.juni;
+    requires amayaframework.tokenize;
+    // Filters
+    requires amayaframework.filter;
+    // Path
+    requires amayaframework.path;
+    requires amayaframework.path.parser;
+    // Router
+    requires amayaframework.router;
+    // Amaya modules
+    requires amayaframework.options;
+    requires amayaframework.web;
     // Exports
     exports io.github.amayaframework.routing;
 }
