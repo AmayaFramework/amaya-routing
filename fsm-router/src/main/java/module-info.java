6f8f7813b9e71c35b21b55546709510c2a177813
Roman Bakaldin <gbakaldin@gmail.com>
2025-09-04 19:02:35 +0300

    Add javadocs for fsm router module-info

--- fsm-router/src/main/java/module-info.java
+++ fsm-router/src/main/java/module-info.java
@@ -1,5 +1,11 @@
 /**
- * TODO
+ * Module providing finite state machine–based routing implementation for Amaya framework.
+ *
+ * <p>This module integrates {@link com.github.romanqed.jsm} state machines
+ * into the Amaya routing system, enabling efficient matching of dynamic paths.
+ * It exposes the {@link io.github.amayaframework.router.fsm} package,
+ * which contains the {@link io.github.amayaframework.router.fsm.MachineRouterFactory}
+ * and related classes.</p>
  */
 module amayaframework.router.fsm {
     // Imports
