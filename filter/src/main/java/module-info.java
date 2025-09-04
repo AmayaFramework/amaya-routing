f693d3846b90ad54c6f69b409e9ce6a6a827bd9a
Roman Bakaldin <gbakaldin@gmail.com>
2025-09-04 15:51:03 +0300

    Add javadoc for module-info

--- filter/src/main/java/module-info.java
+++ filter/src/main/java/module-info.java
@@ -1,3 +1,20 @@
+/**
+ * Provides filtering utilities for Amaya Framework.
+ * <p>
+ * This module defines the {@link io.github.amayaframework.filter.Filter}
+ * abstraction for processing raw string values into typed objects,
+ * as well as the {@link io.github.amayaframework.filter.FilterSet}
+ * API for managing collections of filters. It also includes
+ * the {@link io.github.amayaframework.filter.MapFilterSet}
+ * implementation.
+ * </p>
+ *
+ * <p>
+ * Typical usage is parameter parsing in routing, where path and query
+ * parameters are automatically converted into typed values using
+ * registered filters.
+ * </p>
+ */
 module amayaframework.filter {
     // Imports
     requires com.github.romanqed.jfunc;
